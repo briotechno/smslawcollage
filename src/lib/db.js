3cf5296dcf05a1205b79ebaf42a24a125de0061a
Mahipat <59689193+vaghela983@users.noreply.github.com>
2025-10-15 14:54:01 +0530

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,11 +2,11 @@ import mysql from "mysql2/promise";
 
 export async function connectDB() {
   const connection = await mysql.createConnection({
-    host: "127.0.0.1",        // localhost works too
-    user: "root",             // your MySQL username
-    password: "",        // your MySQL password
-    database: "smslawcollage", // the DB you want to use
-    port: 3306
+    host: "p3plzcpnl508454.prod.phx3.secureserver.net",        // localhost works too
+    user: "briotechno",             // your MySQL username
+    password: "Briotechno@983",        // your MySQL password
+    database: "SMS_COLLAGE", // the DB you want to use
+    port: 2083
   });
   return connection;
 }
