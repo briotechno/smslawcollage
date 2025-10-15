dbac6958c94e4e85fa82499f9aab81274d6a49a3
Mahipat <59689193+vaghela983@users.noreply.github.com>
2025-10-15 17:25:48 +0530

    Update route.ts

--- src/app/api/db/route.ts
+++ src/app/api/db/route.ts
@@ -1,7 +1,7 @@
 import { connectDB } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function GET(request?: Request) {
+export async function GET(request: Request) {
   const start = Date.now();
 
   try {
